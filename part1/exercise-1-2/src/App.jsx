c4c11a0c7c68e4ca0d1b19f0b1ef0754aa701900
MauricioMunguia10 <mauriciomunguiagarcia@gmail.com>
2024-05-03 20:01:49 -0600

    finish exercise 2

--- part1/exercise-1-2/src/App.jsx
+++ part1/exercise-1-2/src/App.jsx
@@ -1,7 +1,22 @@
-/*make three components 
+/*first part 1.1
+make three components 
 header --> name of the course
 content --> parts and numbers of of exercises
 total --> total number of exercises*/
+
+/*second part 1.2
+refactor the content component to renders three Part components*/
+
+const Part = (exercise) => {
+  console.log(exercise)
+  return(
+    <>
+      <p>
+        {exercise.part} {exercise.exercise}
+      </p>
+    </>
+  )
+} 
 
 const Header = (course) => {
   console.log(course)
@@ -22,17 +37,11 @@ const Content = () => {
   const part3 = 'State of a component'
   const exercises3 = 14
 
-  return(
+  return (
     <div>
-     <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercise={exercises1}/>
+      <Part part={part2} exercise={exercises2}/>
+      <Part part={part3} exercise={exercises3}/>
     </div>
   )
 }
