d2c4e45b8a9b3fdd620b2f4eeab3d02f8d509567
MauricioMunguia10 <mauriciomunguiagarcia@gmail.com>
2024-05-07 13:23:04 -0600

    part 1.4

--- part1/exercises/src/App.jsx
+++ part1/exercises/src/App.jsx
@@ -7,7 +7,7 @@ total --> total number of exercises*/
 /*second part 1.2
 refactor the content component to renders three Part components*/
 
-const Part = (exercise) => {
+/*const Part = (exercise) => {
   //console.log(exercise)
   return(
     <>
@@ -17,68 +17,84 @@ const Part = (exercise) => {
     </>
   )
 } 
-
+*/
 const Header = (course) => {
   //console.log(course)
   return(
-    <div>
+    <>
       <h1>
         {course.course}
       </h1>
-    </div>
+    </>
   )
 }
 
-const Content = (part) => {
+const Content = (content) => {
   // const part1 = 'Fundamentals of React'
   // const exercises1 = 10
   // const part2 = 'Using props to pass data'
   // const exercises2 = 7
   // const part3 = 'State of a component'
   // const exercises3 = 14
-  let a = part
-  //console.log(a['parts']['name'])
-  return (
-    <div>
-      <Part part={a['parts']['name']} exercise={a['parts']['exercises']}/>
-    </div>
+  let array = content.parts
+  return(
+    <>
+      <p>{array[0]['name']} {array[0]['exercises']}</p>
+      <p>{array[1]['name']} {array[1]['exercises']}</p>
+      <p>{array[2]['name']} {array[2]['exercises']}</p>
+    </>
   )
+  
+  
+
+  
 }
 
-const Total = () => {
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
+const Total = (total) => {
+  // const exercises1 = 10
+  // const exercises2 = 7
+  // const exercises3 = 14
+  let arr = total.parts
+  let sum=0
+  for(let i=0;i<arr.length;i++){
+    //console.log(arr)
+    sum+=arr[i]['exercises']
+  }
   return(
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <>
+      <p>Total exercises: {sum}</p>
+    </>
   )
 }
 
 /* part 1.3 Modify the variable definitions of the App component as 
 follows and also refactor the application so that it still works: */
 
+/* part 1.4 Place the objects into an array. Modify the variable definitions of App 
+into the following form and modify the other parts of the application accordingly*/
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={part1} />
-      <Content parts={part2} />
-      <Content parts={part3} />
-      <Total />
+      <Content parts={parts} />
+      <Total parts={parts}/>
     </div>
   )
 }
