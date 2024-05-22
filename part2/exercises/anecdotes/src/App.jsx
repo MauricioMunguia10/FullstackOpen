9c585bbf7d9fb654cca4b1fd6774175fb216fe5e
Mauricio Munguia <mauriciomunguiagarcia@gmail.com>
2024-05-22 10:35:02 -0600

    exercise 2.4

--- part2/exercises/anecdotes/src/App.jsx
+++ part2/exercises/anecdotes/src/App.jsx
@@ -5,6 +5,9 @@ Show also the sum of the exercises of the course.
 
 2.3*: Course information step8
 If you haven't done so already, calculate the sum of exercises with the array method reduce.
+
+2.4: Course information step9
+Let's extend our application to allow for an arbitrary number of courses
 */
 
 
@@ -67,29 +70,58 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return(
+    <>
+      {courses.map(course => 
+          <Course course={course} />
+      )}
+    </>
+  )
 }
 
 export default App
