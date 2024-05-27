d9a6ad0bb304e8f86eb2eeb0c9105ba7a0a452bb
MauricioMunguia10 <mauriciomunguiagarcia@gmail.com>
2024-05-26 19:20:16 -0600

    exercise 2.7

--- part2/exercises/phoneBook/src/App.jsx
+++ part2/exercises/phoneBook/src/App.jsx
@@ -4,16 +4,29 @@ import { Title } from './components/Basics'
 import Result from './components/Results'
 
 /*2.6: The Phonebook Step 1
-Let's create a simple phonebook. In this part, we will only be adding names to the phonebook.*/
+Let's create a simple phonebook. In this part, we will only be adding names to the phonebook.
+
+2.7: The Phonebook Step 2
+Prevent the user from being able to add names that already exist in the phonebook.
+JavaScript arrays have numerous suitable methods for accomplishing this task.
+Keep in mind how object equality works in Javascript.
+*/
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ])
+  
   const handleDataChange = (newPerson) => {
-    const person = { name: newPerson}
-    const updatedPersons = [...persons, person];
-    setPersons(updatedPersons);
+    const result = persons.find((person) => person.name === newPerson)
+    if(result)
+      alert(`${newPerson} is already added to phonebook`)
+    else{
+      const person = { name: newPerson}
+      const updatedPersons = [...persons, person];
+      setPersons(updatedPersons);
+    }
+    
   };
 
   return (
