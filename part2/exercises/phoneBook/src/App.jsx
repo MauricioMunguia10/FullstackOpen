e544c8665ca32b8cdb683b4adefe31a601ffdb74
Mauricio Munguia <mauriciomunguiagarcia@gmail.com>
2024-05-27 12:25:17 -0600

    exercise 2.9

--- part2/exercises/phoneBook/src/App.jsx
+++ part2/exercises/phoneBook/src/App.jsx
@@ -12,11 +12,17 @@ JavaScript arrays have numerous suitable methods for accomplishing this task.
 Keep in mind how object equality works in Javascript.
 
 2.8: The Phonebook Step 3
-Expand your application by allowing users to add phone numbers to the phone book. */
+Expand your application by allowing users to add phone numbers to the phone book.
+
+2.9*: The Phonebook Step 4
+Implement a search field that can be used to filter the list of people by name*/
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNumber: 5617801020 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const handleDataChange = (newPerson) => {
@@ -33,7 +39,8 @@ const App = () => {
 
   return (
     <>
-      <Title text='Phonebook' type='h2' />
+      <Title text='Phonebook' type='h1' />
+      <Title text='Add a new one' type='h2' />
       <Form onDataChange={handleDataChange} />
       <Title text='Numbers' type='h2' />
       <Result persons={persons} />
--- part2/exercises/phoneBook/src/components/Results.jsx
+++ part2/exercises/phoneBook/src/components/Results.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react"
+
 const Result = ({persons}) => {
+
+  const [inputFilter, setInputFilter] = useState('')
+
+  const handleFilterChange = (event) => {
+    setInputFilter(event.target.value)
+
+  }
+
+  const filterPerson = (filter) => {
+    return persons.filter((person) =>
+      person.name.toLowerCase().startsWith(filter.toLowerCase())
+    )
+  }
+
   return(
     <>
-      {persons.map((person,i) =>{
+      <input 
+        type="text" 
+        placeholder="Filter by" 
+        value={inputFilter} 
+        onChange={handleFilterChange} />
+
+      {filterPerson(inputFilter).map((person,i) =>{
         return <p key={i}>{person.name} {person.phoneNumber}</p>
       })}
     </>
